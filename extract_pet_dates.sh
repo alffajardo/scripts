cef854931581d6fc7304a9f8deb82dcc04a846b7
alffajardo <faj.alf@gmail.com>
2023-01-31 12:12:44 -0500

    minor corrections to extract dates pet script

--- extract_pet_dates.sh
+++ extract_pet_dates.sh
@@ -14,16 +14,13 @@ session='ses-01'
 
 
 
-
-
-
-dates=$(ls *NAV*ses-01*| rev | cut -d _ -f 1 | rev)
+date=$( ls | grep $session | grep $tracer | grep $subject_id  | rev | cut -d _ -f 1 | rev)
 
 # Fix the dates the old school
 
-fixed_dates=$(echo $dates | sed "s/Jan/-01-/g" |     sed "s/Feb/-02-/g" |  sed "s/Mar/-03-/g" |    sed "s/Apr/-04-/g" |      sed "s/May/-05-/g" | sed "s/Jun/-06-/g" | sed "s/Jul/-07-/g" |     sed "s/Aug/-08-/g" |     sed "s/Sep/-09-/g" |     sed "s/Oct/-10-/g" |   sed "s/Nov/-11-/g" |sed "s/Dec/-12-/g")
+fixed_date=$(echo $date | sed "s/Jan/-01-/g" |     sed "s/Feb/-02-/g" |  sed "s/Mar/-03-/g" |    sed "s/Apr/-04-/g" |      sed "s/May/-05-/g" | sed "s/Jun/-06-/g" | sed "s/Jul/-07-/g" |     sed "s/Aug/-08-/g" |     sed "s/Sep/-09-/g" |     sed "s/Oct/-10-/g" |   sed "s/Nov/-11-/g" |sed "s/Dec/-12-/g")
 
-for d in $fixed_dates
+for d in $fixed_date
 do
 day=$(echo $d | cut -d '-' -f 1)
 month=$(echo $d | cut -d '-' -f 2)
