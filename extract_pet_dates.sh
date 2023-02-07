b582b3ae5240ccd7b9117d947a4fc57e98685aa4
alffajardo <faj.alf@gmail.com>
2023-02-07 11:30:50 -0500

    modified script to extract pet dates

--- extract_pet_dates.sh
+++ extract_pet_dates.sh
@@ -20,15 +20,16 @@ date=$( ls | grep $session | grep $tracer | grep $subject_id  | rev | cut -d _ -
 
 fixed_date=$(echo $date | sed "s/Jan/-01-/g" |     sed "s/Feb/-02-/g" |  sed "s/Mar/-03-/g" |    sed "s/Apr/-04-/g" |      sed "s/May/-05-/g" | sed "s/Jun/-06-/g" | sed "s/Jul/-07-/g" |     sed "s/Aug/-08-/g" |     sed "s/Sep/-09-/g" |     sed "s/Oct/-10-/g" |   sed "s/Nov/-11-/g" |sed "s/Dec/-12-/g")
 
-for d in $fixed_date
-do
-day=$(echo $d | cut -d '-' -f 1)
-month=$(echo $d | cut -d '-' -f 2)
-year=$(echo $d | cut -d '-' -f 3)
 
-echo $year-$month-$day
+day=$(echo $fixed_date | cut -d '-' -f 1)
+month=$(echo $fixed_date | cut -d '-' -f 2)
+year=$(echo $fixed_date | cut -d '-' -f 3)
 
-done
+echo $subject_id $year-$month-$day
+
+
+
+
 
 
 exit
