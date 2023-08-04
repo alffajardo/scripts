68268a20562cffcc4c983e67db5a322b3dbc9731
alffajardo <faj.alf@gmail.com>
2023-08-04 15:18:58 -0400

    added a transperency option

--- overlay_pet.R
+++ overlay_pet.R
@@ -2,7 +2,7 @@
 
 library(neurobase)
 library(sommer)
-
+library(scales)
  args<- commandArgs(trailingOnly = T )
  print(args)
  
@@ -17,6 +17,9 @@ library(sommer)
 
 anat <- readNIfTI( fname = anat_file, reorient = F)
 pet <- readNIfTI(pet_file,reorient = F)
+
+#anat <- readNIfTI("anat/sub-CD001_TAU_T1w.nii.gz")
+#pet <- readNIfTI("pet/sub-CD001_TAU_pet_time-4070_space-anat_ref-infcereg_suvr.nii.gz")
 pet[is.nan(pet)] <- 0
 
 max.pet <- max(pet)
@@ -28,7 +31,8 @@ png(output,height = 1100,width = 1100,res = 100,units = "px",)
 
 
 
-ortho2(x = anat, y = pet, xyz = coords,crosshairs = F,zlim.y = c(0.5,3), col.y = jet.colors(100),
+ortho2(x = anat, y = pet, xyz = coords,crosshairs = F,zlim.y = c(0.5,3.35), 
+       col.y = alpha(jet.colors(100),.2),
        mar = c(0,0,0,0), bg = "black",NA.x = T,NA.y = F)
 dev.off()
 
