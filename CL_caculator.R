cd6bb9ba1a5a0eef5c1ef15e7bfd4c2d3bbb4aee
alffajardo <faj.alf@gmail.com>
2024-11-20 14:48:16 -0500

    created a CL calculator based on the prevent-ad Data

--- CL_caculator.R
+++ CL_caculator.R
@@ -1,41 +1,67 @@
-# functions to transform 
+#!/usr/bin/Rscript
 
-cl2suvr <- function (CL_value = NULL,intercept = 1.031, slope = 1.172){
+# Function to convert Centiloid (CL) to SUVR
+cl2suvr <- function(CL_value = NULL, intercept = 1.031, slope = 1.172) {
   
-  if (is.null(CL_value)){
-    
+  if (is.null(CL_value)) {
     stop("Please provide a Centiloid value")
-    
-  
   }
   
-  
-  SUVR <- intercept + ((CL_value*slope)/100)
+  SUVR <- intercept + ((CL_value * slope) / 100)
   
   return(SUVR)
 }
 
-
-
-
-
-suvr2cl <- function (suvr_value = NULL,intercept = 1.031, slope = 1.172){
+# Function to convert SUVR to Centiloid (CL)
+suvr2cl <- function(suvr_value = NULL, intercept = 1.031, slope = 1.172) {
   
-  if (is.null(suvr_value)){
-    
+  if (is.null(suvr_value)) {
     stop("Please provide an SUVR value")
-    
-    
   }
   
-  
-  CL <- 100*((suvr_value - intercept) / slope)
+  CL <- 100 * ((suvr_value - intercept) / slope)
   
   return(CL)
 }
 
+# Function to convert Gray Matter SUVR to Centiloid (CL)
+gmsuvr2CL <- function(suvr_value = NULL) {
+  
+  if (is.null(suvr_value)) {
+    stop("Please provide a Gray Matter SUVR value")
+  }
+  
+  # Adjusted formula with the updated intercept and slope
+  suvr_corr <- -0.0246 + (suvr_value * 0.796)
+  CL <- suvr2cl(suvr_corr)
+  
+  return(CL)
+}
 
-## In the Villenueve lab we use gray matter cerebellum as a reference
-## region. Ever wondered the equivalent in Prevent AD SUVrs to WC CLs?
+# Function to convert Centiloid (CL) to Gray Matter SUVR
+CL2gmsuvr <- function(cl_value = NULL) {
+  
+  if (is.null(cl_value)) {
+    stop("Please provide a Centiloid (CL) value")
+  }
+  
+  # Convert CL to corrected SUVR using cl2suvr
+  suvr_corr <- cl2suvr(cl_value)
+  
+  # Apply the inverse correction formula with the updated intercept and slope
+  suvr_value <- (suvr_corr + 0.0246) / 0.796
+  
+  return(suvr_value)
+}
+
+## In the Villeneuve lab, we use gray matter cerebellum as a reference
+## region. Ever wondered the equivalent in Prevent AD SUVRs to WC CLs?
 # This function promises to solve the inquiry.
+
+
+### 
+
+vals <- seq(1,2.9,by = 0.01)
+plot(vals,gmsuvr2CL(vals), type = "l",)
+abline(v = 1.25,col = "red", lty = 2)
 
